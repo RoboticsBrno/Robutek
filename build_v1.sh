dc6b7098c3fe5a41f8b23e1d43fc1898711215fb
C2Coder <jansky.stepan08@gmail.com>
2025-06-17 01:54:13 +0200

    feat: make better build scripts

---
+++ build_v1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+echo "Building V1..."
+mkdocs build --config-file mkdocs.v1.yml
---
+++ build_v2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+echo "Building V2..."
+mkdocs build --config-file mkdocs.v2.yml
