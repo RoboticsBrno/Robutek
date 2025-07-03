56d68b0b767f6445ef9826ef5d362fb3b3fad680
Giraffe8896 <gamemail.parzival@centrum.cz>
2025-07-03 18:53:50 +0200

    edit: new example code

--- docs/v2/robot/lekceGridUI/colors-gridui/src/index.ts
+++ docs/v2/robot/lekceGridUI/colors-gridui/src/index.ts
@@ -9,46 +9,33 @@ const robutek = createRobutek("V2");
 const ledStrip = new SmartLed(48, 1, LED_WS2812);  // připojí pásek na pin 48, s 1 ledkou a typem WS2812
 
 // Změň mě!
-const OWNER = "owner";
+const OWNER = "Parz";
 const DEVICE_NAME = "Robutek";
 
-let text = "#ffffff";
-let r = "ff"
-let g = "ff"
-let b = "ff"
+let text = "rgb(0 0 0)"
 
-function colorToHexString(color:colors.Rgb) { //překládá barvu na string požadovaný GridUI (#ffffff)
-    r =  Math.trunc(color.r).toString(16)
-    g = Math.trunc(color.g).toString(16)
-    b = Math.trunc(color.b).toString(16)
-    if (r.length === 1) {
-        r = "0" + r;
-    } 
-    if (g.length === 1) {
-        g = "0" + g;
-    } 
-    if (b.length === 1) {
-        b = "0" + b;
-    }
-    text = "#" + r + g + b;
-    return text
+function colorToRgbString(col:colors.Rgb){ // překládá rgb barvu na string pro GridUI
+    return "rgb(" + Math.trunc(col.r) + " " + Math.trunc(col.g) + " " + Math.trunc(col.b) + ")"
 }
 
 setInterval(() => {
     Layout.Led.on = !Layout.Led.on; // bliká ledkou na rozhraní GridUI
+    console.log("blik")
 }, 500);
 
 
 Layout.begin(OWNER, DEVICE_NAME, builder => {
     builder.SetColor.onChanged(slider => {
-        ledStrip.set(0, colors.rainbow(slider.value)) // nastavuje barvu ledky na desce
-        ledStrip.show()
-        text = colorToHexString(colors.rainbow(slider.value));
+        const col = colors.rainbow(slider.value)
+        ledStrip.set(0, col); // nastavuje barvu ledky na desce
+        ledStrip.show();
+
+        text = colorToRgbString(col);
         Layout.Led.color = text; // nastavuje barvu ledky na rozhraní
         Layout.Text.text = text; // vypisuje rgb kód v textovém poli na rozhraní
         
     })
 });
 
-//console.log("Otevři aplikaci RBController nebo otevři prohlížeč a zadej IP: http://" + wifi.currentIp() + " pro ovládání robota.");
+console.log("Otevři aplikaci RBController nebo otevři prohlížeč a zadej IP: http://" + wifi.currentIp() + " pro ovládání robota.");
 Layout.SetColor.value = 180
